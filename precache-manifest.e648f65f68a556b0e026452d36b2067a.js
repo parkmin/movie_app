af2a1c189edf6ff028144c7bf22adab4798c9f5e
mpark <minmin7894@gmail.com>
2018-12-03 22:00:21 +0900

    Updates

---
+++ precache-manifest.e648f65f68a556b0e026452d36b2067a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "033d892a9643a0867e69",
+    "url": "/movie_app/static/js/main.033d892a.chunk.js"
+  },
+  {
+    "revision": "0bee8e5dcb3c5e319809",
+    "url": "/movie_app/static/js/1.0bee8e5d.chunk.js"
+  },
+  {
+    "revision": "033d892a9643a0867e69",
+    "url": "/movie_app/static/css/main.d5db5ccc.chunk.css"
+  },
+  {
+    "revision": "bd380df3b9b425327459b2ecac80ab72",
+    "url": "/movie_app/index.html"
+  }
+];
